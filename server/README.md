d41e0f6fd761b7d9521ad208d25df72708354c0c
Joshua Ji <joshua.ji@jiwhiz.com>
2023-01-07 21:45:13 +0000

    Improve docker stuff

--- server/README.md
+++ server/README.md
@@ -1,3 +1,9 @@
 # Server
 
 The server serves as a gateway to all the services that the frontend needs to access. It is a simple flask server.
+
+## Running
+
+```bash
+cd server
+flask --debug run
--- server/app/__init__.py
+++ server/app/__init__.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
 @app.route('/upload-tex', methods=['POST'])
 def upload():
     file = request.files['file']
