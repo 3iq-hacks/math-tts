f8715164a7f0027f0b3ebeb3e176ed0858976c6b
Joshua Ji <joshua.ji@jiwhiz.com>
2023-01-08 00:41:04 +0000

    server: add some comments

--- server/main.py
+++ server/main.py
@@ -11,11 +11,17 @@ async def root():
 
 @app.post('/upload-tex')
 async def upload_tex(file: UploadFile = File(...)):
+    '''
+    Takes in a `.tex` file, and returns the `.mp3` of the tex
+    '''
     return 'upload tex file'
 
 
 @app.post('/upload-img')
 async def upload_image(file: UploadFile = File(...)):
+    '''
+    Receives an image of a LaTeX equation, and returns the latex
+    '''
     # check that the file is an image
     if file.filename and allowed_file(file.filename):
         await pix2tex.load_model()
@@ -23,10 +29,13 @@ async def upload_image(file: UploadFile = File(...)):
         print(latex)
 
         return {'latex': latex}
-    else:
-        return {'error': 'file not allowed'}
+
+    return {'error': 'file not allowed'}
 
 
 def allowed_file(filename: str):
+    '''
+    Checks if a file is "allowed" by seeing its extension
+    '''
     ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
     return '.' in filename and filename.rsplit('.')[-1].lower() in ALLOWED_EXTENSIONS
