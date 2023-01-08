870736bcb0afaafd653d7615abfc72c3465787b0
Joshua Ji <joshua.ji@jiwhiz.com>
2023-01-08 15:01:55 +0000

    update node version

--- server/main.py
+++ server/main.py
@@ -8,12 +8,17 @@ port = os.environ.get('PORT', 8000)
 
 app = FastAPI(port=port)
 
+allowed_origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
 
