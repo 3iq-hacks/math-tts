44a1dfbf7032d39fef252e5f4176fde87c2a90e6
Joshua Ji <joshua.ji@jiwhiz.com>
2023-01-18 08:24:05 +0000

    Start nextjs migration

---
+++ new-client/pages/index.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center dark:bg-slate-800">
+                <h1 className="text-6xl font-bold dark:text-slate-100">
+                    Latex 2 <span className="text-blue-600 dark:text-indigo-400">Speech</span>
+                </h1>
+            </main>
+
+            <footer className="flex h-24 w-full items-center justify-center border-t dark:bg-slate-800">
+                <a
+                    className="flex items-center justify-center gap-2 "
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                </a>
+            </footer>
+        </div>
+    )
+}
+
+export default Home
