0e21c360bf18169729d4b8cce2db0ee5f500b48f
Joshua Ji <joshua.ji@jiwhiz.com>
2023-01-21 21:30:36 +0000

    Basic file upload

--- new-client/pages/index.tsx
+++ new-client/pages/index.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Lato } from '@next/font/google'
+import FileUpload from '@/components/FileUpload'
+
+const lato = Lato({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+})
 
 const Home: NextPage = () => {
     return (
@@ -10,15 +17,17 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center dark:bg-slate-800">
-                <h1 className="text-6xl font-bold dark:text-slate-100">
+            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 spac text-center dark:bg-slate-800">
+                <h1 className="text-6xl font-bold dark:text-slate-100 ">
                     Latex 2 <span className="text-blue-600 dark:text-indigo-400">Speech</span>
                 </h1>
+                <p className={`text-lg dark:text-slate-100`}>Upload a LaTeX file (.tex) or image file (.png, .jpeg, .jpg) to get started!</p>
+                <FileUpload />
             </main>
 
-            <footer className="flex h-24 w-full items-center justify-center border-t dark:bg-slate-800">
+            <footer className="flex h-24 w-full items-center justify-center border-t dark:border-slate-600 dark:bg-slate-800">
                 <a
-                    className="flex items-center justify-center gap-2 "
+                    className="flex items-center justify-center gap-2 dark:text-slate-100"
                     href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                     target="_blank"
                     rel="noopener noreferrer"
