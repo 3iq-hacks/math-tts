cd7a17e59423dffeefa2d37a72f56ecb814ab226
Joshua Ji <joshua.ji@jiwhiz.com>
2023-02-02 23:07:33 +0000

    Minor changes

--- new-client/components/FileUpload.tsx
+++ new-client/components/FileUpload.tsx
@@ -179,44 +179,11 @@ const UploadButton = () => {
     )
 }
 
-const DisplayResponse = () => {
-    const { state } = useXState();
-
-    if (state.matches('GotResponse')) {
-        const response = state.context.response;
-
-        if (response.tag === 'error') {
-            return (
-                <div className="flex flex-col items-center justify-center w-full p-4 space-y-1">
-                    <p className="text-xl text-red-500">Error uploading file!</p>
-                    <p className="text-sm font-light text-slate-400">{response.error}</p>
-                    <p className="text-sm font-light text-slate-50">Please check the console for more information!</p>
-                </div>
-            )
-        }
-
-        // success!
-        // show latex, english, and mp3 file (which is represented in base64)
-        return (
-            <div className="flex flex-col items-center justify-center w-full p-4 space-y-1">
-                <p className="text-xl text-green-500">File uploaded successfully!</p>
-                <p className="text-sm font-light text-slate-400">Latex: {response.latex}</p>
-                <p className="text-sm font-light text-slate-400">English: {response.english}</p>
-                {/* http://www.iandevlin.com/blog/2012/09/html5/html5-media-and-data-uri/ */}
-                <ReactAudioPlayer src={`data:audio/mp3;base64,${response.mp3}`} controls />
-            </div>
-        )
-    }
-
-    return null;
-}
-
 const FileUpload: React.FC = () => {
     return (
         <div className="flex items-center justify-center w-full flex-col space-y-6">
             <DragAndDropSection />
             <UploadButton />
-            <DisplayResponse />
         </div>
     )
 }
--- new-client/pages/index.tsx
+++ new-client/pages/index.tsx
@@ -3,8 +3,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Lato } from '@next/font/google'
 import FileUpload from '@/components/FileUpload'
-import { useXState } from '../lib/StateMachineContext'
+import { useXState } from '@/lib/StateMachineContext'
 import Toaster from '@/components/Toaster'
+import DisplayResponse from '@/components/DisplayResponse'
+import Footer from '@/components/Footer'
+
 
 const lato = Lato({
     subsets: ['latin'],
@@ -21,29 +24,18 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 space-y-4 text-center dark:bg-slate-800">
+            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 py-10 space-y-6 text-center dark:bg-slate-800">
                 <h1 className="text-6xl font-bold dark:text-slate-100 ">
                     Latex 2 <span className="text-blue-600 dark:text-indigo-400">Speech</span>
                 </h1>
                 <p className={`text-lg dark:text-slate-100`}>Upload a LaTeX file (.tex) or image file (.png, .jpeg, .jpg) to get started!</p>
-                <p className='text-md text-slate-50'>{state.value.toString()}</p>
                 <FileUpload />
+                <DisplayResponse />
             </main>
 
             {/* Display toasts on the bottom right */}
             <Toaster />
-
-            <footer className="flex h-24 w-full items-center justify-center border-t dark:border-slate-600 dark:bg-slate-800">
-                <a
-                    className="flex items-center justify-center gap-2 dark:text-slate-100"
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                </a>
-            </footer>
+            <Footer />
         </div>
     )
 }
